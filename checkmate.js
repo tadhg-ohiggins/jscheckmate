d963cdf661cd09b6f0986d7afb5ecaf9da2ad485
Tadhg O'Higgins <tadhg@snaptic.com>
2012-11-08 17:48:23 -0800

    Basic functionality working; no visible indication of bounding area apart from confusing text selection highlighting.

--- checkmate.js
+++ checkmate.js
@@ -1,17 +1,83 @@
 (function () {
     var checkMate = {
         init: function() {
-            // Add Google CDN jQuery:
-            jqEl = document.createElement("script");
-            jqEl.setAttribute("type", "text/javascript");
-            jqEl.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js");
-            document.getElementsByTagName("head")[0].appendChild(jqEl)
-            this.downer();
+            document.addEventListener("mousedown", checkMate.downer, true);
+            console.log("yo");
         },
         downer: function(e) {
-            alert("downer", e);
+            console.log("downer");
+            console.log(e);
+            checkMate.start_x = e.pageX;
+            checkMate.start_y = e.pageY;
+            document.addEventListener("mouseup", checkMate.upper, true);
+            document.removeEventListener("mousedown", checkMate.downer, true);
+        },
+        upper: function(e) {
+            console.log("upper");
+            console.log(e);
+            checkMate.end_x = e.pageX;
+            checkMate.end_y = e.pageY;
+            checkMate.rectangle = {};
+            if (checkMate.end_x > checkMate.start_x) {
+                checkMate.rectangle.left = checkMate.start_x;
+                checkMate.rectangle.right = checkMate.end_x;
+            } else {
+                checkMate.rectangle.left = checkMate.end_x;
+                checkMate.rectangle.right = checkMate.start_x;
+            };
+            if (checkMate.end_y > checkMate.start_y) {
+                checkMate.rectangle.top = checkMate.start_y;
+                checkMate.rectangle.bottom = checkMate.end_y;
+            } else {
+                checkMate.rectangle.top = checkMate.end_y;
+                checkMate.rectangle.bottom = checkMate.start_y;
+            };
+            document.removeEventListener("mouseup", checkMate.upper, true);
+            checkMate.calculate();
+        },
+        calculate: function() {
+            var checkboxes = checkMate.getCheckboxes();
+            console.log(checkMate.start_x, checkMate.start_y, checkMate.end_x, checkMate.end_y);
+            for (var i=0; i<checkboxes.length; i++) {
+                this_box = checkboxes[i];
+                newVal = !this_box.inp.getAttribute("checked");
+                this_box.inp.checked = newVal;
+                this_box.inp.setAttribute("checked", newVal);
+            }
+
+        },
+        getCheckboxes: function() {
+            var inputs = document.getElementsByTagName("input");
+            var checkboxes = [];
+            for (var i=0; i<inputs.length; i++) {
+                this_input = inputs[i];
+                console.log(this_input.type);
+                if (this_input.type == "checkbox") {
+                    var coords = checkMate.offset(this_input);
+                    console.log(coords, checkMate.rectangle);
+                    if (coords.left > checkMate.rectangle.left && coords.left < checkMate.rectangle.right) {
+                        console.log(coords.left, checkMate.rectangle);
+                        if (coords.top > checkMate.rectangle.top && coords.top < checkMate.rectangle.bottom) {
+                            console.log("inserting");
+                            checkboxes.push({coords: coords, inp: this_input});
+                        }
+                    }
+                }
+            }
+
+            window["checkboxes"] = checkboxes;
+            return checkboxes;
+
+        },
+        offset: function(elm){
+            var obj = elm.getBoundingClientRect();
+            return {
+                left: obj.left + document.body.scrollLeft,
+                top: obj.top + document.body.scrollTop,
+                width: obj.width,
+                height: obj.height
+            };
         }
     };
     checkMate.init();
 })();
-
